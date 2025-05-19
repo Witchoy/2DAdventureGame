07673409b27be9d52068b5883f89b6f4bdbddb87
j.goy <julesgoydev@gmail.com>
2025-05-19 19:18:01 +0200

    Unit : Player character and movement finished !

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewEmptyCSharpScript : MonoBehaviour
 {
+    public InputAction moveAction;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.05F;
+        Vector2 move = moveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0F * Time.deltaTime;
         transform.position = position;
     }
-    
 }
