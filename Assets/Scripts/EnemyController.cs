594c5a64fe7d7a0d8ddbb84d2d5a0c868571dc4a
j.goy <julesgoydev@gmail.com>
2025-06-07 14:33:47 +0200

    Tuto "Add an enemy character" finished !

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+
+
+public class EnemyController : MonoBehaviour
+{
+    // Public variables
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0F;
+    public int damage = 1;
+
+    // Private variables
+    Rigidbody2D rigidbody2d;
+    float timer;
+    int direction = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    // Update is called every frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        if (vertical)
+        {
+            position.y = position.y + speed * direction * Time.deltaTime;
+        }
+        else
+        {
+            position.x = position.x + speed * direction * Time.deltaTime;
+        }
+        rigidbody2d.MovePosition(position);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            Debug.Log("Collision entenred !");
+            player.ChangeHealth(-damage);
+        }
+        else
+        {
+            Debug.Log("Player is null ! " + collision.ToString() );
+        }
+    }
+}
